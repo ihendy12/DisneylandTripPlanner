7ad6cae5f78b890061e03eaa0c3efc1454489301
Ian Henderson <ian.henderson1215@gmail.com>
2023-03-17 12:59:11 -0700

    wrote brief outline of project

---
+++ src/main/java/org/example/LogicLayout.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class LogicLayout {
+    public static void main(String[] args) {
+
+        //Goals for project
+        // 1. Make an itinerary for a persons day at Disneyland based on the averaging the prior 3 years data to find the
+        // shortest wait-time per 30 minute blocks
+
+        // Will need to make sure we are finding an API or database that has historical wait times for Disneyland
+        // Will need input from customer on the following: Day they are visiting, time they want to start, time they want
+        // to end, if they want to include food and snack breaks (if so, how many), and if they want to watch
+        // a parade, fireworks, or Fantasmic!, or all 3
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Disneyland Trip Planner!");
+        System.out.println("We will start by asking some questions about your upcoming trip");
+        System.out.println("What day will you be visiting? ");
+
+
+
+    }
+}
